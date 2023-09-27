db4b9af958cc08cf9f014011774fb23a5f16b8e5
Rafael Leonardo <rafaelleonardodasilva.dv@gmail.com>
2023-09-27 14:23:28 -0300

    Arrumado erro ao aceesar books e book instances no admin

--- catalog/models.py
+++ catalog/models.py
@@ -11,6 +11,12 @@ class Genre(models.Model):
         return self.name
 
 
+class Language(models.Model):
+    language = models.CharField(max_length=100, help_text='Coloque a língua natural do livro. (Ex: Português')
+
+    def __str__(self):
+        return self.language
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
@@ -19,7 +25,7 @@ class Book(models.Model):
                             help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre,
                                    help_text='Selecione um gênero para esse livro')
-    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
@@ -72,8 +78,4 @@ class Author(models.Model):
         return f'{self.last_name}, {self.first_name}'
 
 
-class Language(models.Model):
-    language = models.CharField(max_length=100, help_text='Coloque a língua natural do livro. (Ex: Português')
 
-    def __str__(self):
-        return self.language
